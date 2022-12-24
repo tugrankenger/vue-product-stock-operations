01fd866a70fbec165d02df6101edc0920524d697
tugrankenger <turankenger@hotmail.com.tr>
2022-12-24 13:41:16 +0300

    created store methods for purchase, sale and balance

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,19 @@
 import { createStore } from 'vuex'
 import product from './modules/product'
+import * as getters from './getters'
+import * as mutations from './mutations'
+import * as actions from './actions'
 
 const store = createStore({
   state:{
-
+    purchase: 0.0,
+    sale: 0.0,
+    balance: 0.0
   },
+  getters,
+  mutations,
+  actions,
+
   modules:{
     product
   }
